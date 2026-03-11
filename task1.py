42ec64c5670245b28548a304190fdaf343af7a6b
hoganallsbrooks342-creator <hogan.allsbrooks342@students.cmcss.net>
2026-03-10 22:46:04 -0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -29,5 +29,56 @@ print(type(e))
 print(f)
 print(type(f))
 f = float(f)
+from data import price
+
+#TODO 1 Run this program
+#Let'S debug.
+#This time the problem is causing an error
+#TODO 2 Comment lines 18 & 19 to allow the program to run.
+
+#TODO 3 print price
+print(price)
+
+#TODO 4 Print the type
+print(type(price))
+
+#TODO 5 Convert to the correct data type and reassign to price.
+price = float(price)
+
+#TODO 6 Uncomment 18 & 19.
+total = price + 5
+print(total)
+from data import a, b, c, d, e, f
+
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+print(a)
+print(type(a))
+a = float(a)
+# b
+print(b)
+print(type(b))
+
+# c
+print(c)
+print(type(c))
+c=int(c)
+
+# d
+print(d)
+print(type(d))
+d = int(d)
+# e
+print(e)
+print(type(e))
+
+# f
+print(f)
+print(type(f))
+f = float(f)
+
 
 
